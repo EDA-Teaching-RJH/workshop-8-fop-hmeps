f9f06ea18b72715062c30d58261f62563b750937
hmeps <hm18@kent.ac.uk>
2026-02-24 13:11:58 +0000

    part 7 completed

--- caclulator.py/test_calculator.py
+++ caclulator.py/test_calculator.py
@@ -2,11 +2,18 @@ from sixthpartCalculator import square
 def main():
  test_square()
 def test_square():
-    assert square(2) == 4
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared is not 4")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared is not 0")
     assert square(3) == 9
     if square(2) != 4:
         print("2 squared was not 4")
     if square(3) != 9:
-        print("3 squared was not 9")
+        print("3 squared was not 9")        
     if __name__ == "__main__":
         main()
---
+++ hello.py/test_hello.py
@@ -0,0 +1,5 @@
+from seventhpartHello import hello
+def test_default():
+ assert hello() == "hello, world"
+def test_argument():
+ assert hello("Robert") == "hello, Robert"
